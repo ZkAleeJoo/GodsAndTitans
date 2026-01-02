7d0cee2ab196bd707e7feb304301eb100f258c4b
ZkAleeJoo <xxaleejooxx@gmail.com>
2026-01-02 12:39:08 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 const modal = document.getElementById("userModal");
 const btns = document.querySelectorAll(".buy-btn");
 const span = document.querySelector(".close-modal");
+const userInput = document.getElementById("mcUsername");
+const userHead = document.getElementById("userHead");
 
 btns.forEach(btn => {
-    btn.onclick = function() {
-        modal.style.display = "block";
-    }
+    btn.onclick = () => modal.style.display = "block";
 });
 
-span.onclick = function() {
-    modal.style.display = "none";
+span.onclick = () => modal.style.display = "none";
+
+window.onclick = (event) => {
+    if (event.target == modal) modal.style.display = "none";
 }
 
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+userInput.oninput = function() {
+    let username = this.value.trim();
+    if (username.length > 2) {
+        userHead.src = `https://mc-heads.net/avatar/${username}/50`;
+    } else {
+        userHead.src = `https://mc-heads.net/avatar/steve/50`;
     }
-}
+};
